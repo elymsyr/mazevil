c79f3f38fdc25a44766f2d07d44b861ea8a57b64
Orhun Eren Yalçınkaya <orhun868@gmail.com>
2024-08-05 20:38:09 +0300

    Update Path Detection in Game

--- Scripts/path_detection_in_game_2.py
+++ Scripts/path_detection_in_game_2.py
@@ -4,26 +4,28 @@ import numpy as np
 import pygetwindow as gw
 from PIL import ImageGrab
 import numpy as np
-from time import sleep
 import cv2
 from array_scale import downscale_binary_array, upscale_binary_array
 
 window_title = 'Mazevil'
-scale_order = [3, 3, 3]
+scale_order = [9, 3]
 
-def path_detection(rgb, downscale_order = [3,3,3], lower_bound = np.array([89,51,53]), upper_bound = np.array([228,166,114])):
+lower_bound = np.array([100, 50, 50])
+upper_bound = np.array([255, 150, 150])
+
+def path_detection(rgb, downscale_order = [3,3,3], lower_bound = np.array([100, 50, 50]), upper_bound = np.array([255, 150, 150])):
     mask = cv2.inRange(rgb, lower_bound, upper_bound)
     binary_array = (mask > 0).astype(np.uint8)
     for downscale in downscale_order:
         binary_array = downscale_binary_array(binary_array, downscale)
     return binary_array
 
-def capture_window(window):
-    bbox = (window.left, window.top, window.right, window.bottom)
+def capture_window(window, top_crop=20, bottom_crop=50):
+    avg = 8
+    bbox = (window.left+avg, window.top+avg+22, window.right-avg, window.bottom-avg)
     img = ImageGrab.grab(bbox)
     matlike_image = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
-    return cv2.cvtColor(matlike_image, cv2.COLOR_BGR2RGB)
-    # return matlike_image
+    return matlike_image[top_crop:-bottom_crop, :]
 
 windows = gw.getWindowsWithTitle(window_title)
 
@@ -39,15 +41,15 @@ for item in windows:
 
 while True:
     window_image = capture_window(window)
-    binary_array = path_detection(rgb=window_image, downscale_order=scale_order)
-
+    binary_array = path_detection(rgb=cv2.cvtColor(window_image, cv2.COLOR_BGR2RGB), downscale_order=scale_order, lower_bound=lower_bound, upper_bound=upper_bound)
+    # print(binary_array.shape) # 14,29
     for scale in scale_order:
         binary_array = upscale_binary_array(binary_array, scale)
 
     cv2.imshow('paths', (binary_array * 255).astype(np.uint8))
     # cv2.imshow('paths', window_image)
 
-    if cv2.waitKey(100) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cv2.destroyAllWindows()
