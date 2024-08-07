66720b241b1d60ace2c342743282c235c4e3dd37
Orhun Eren Yalçınkaya <orhun868@gmail.com>
2024-08-08 00:52:26 +0300

    Update testmodel01

--- YOLO Model/Model/ready_colab_train.py
+++ YOLO Model/Model/ready_colab_train.py
@@ -2,11 +2,7 @@ import yaml, requests
 import shutil, os
 from data_create import move_files
 
-def represent_scalar_no_quotes(dumper, data):
-    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style=None)
-
-yaml.add_representer(str, represent_scalar_no_quotes)
-
+shutil.rmtree('YOLO Model\\Model\\testdata01')
 
 yolo_model = 'yolov7x'
 model = 'testdata01'
@@ -25,24 +21,19 @@ val = f"YOLO Model\\Model\\{model}\\val"
 with open(f'{model_path}\\classes.txt', 'r') as file:
     class_names = [line.strip() for line in file.readlines()]
 
-class_names_str = "["
-for name in class_names[:-1]:
-    class_names_str += f"{name}, "
-class_names_str += f"{class_names[-1]}]"
-
 nc = len(class_names)
 
 # Configuration details
 config = {
-    'names': f'[{", ".join(class_names)}]',  # list of class names
+    'names': class_names,  # list of class names
     'nc': nc,  # number of classes
-    'val': f"{model}\\val",  # path to validation dataset
-    'train': f"{model}\\train",  # path to training dataset
+    'val': f"./data/val",  # path to validation dataset
+    'train': f"./data/train",  # path to training dataset
 }
 
 # Save the configuration to a YAML file
 with open(f'YOLO Model\\Model\\{model}\\{model.lower()}_config.yaml', 'w') as file:
-    yaml.dump(config, file, default_flow_style=False)
+    yaml.safe_dump(config, file)
 
 url = f'https://raw.githubusercontent.com/WongKinYiu/yolov7/main/cfg/training/{yolo_model}.yaml'
 response = requests.get(url)
@@ -54,4 +45,4 @@ config = yaml.safe_load(response.text)
 config['nc'] = nc 
 
 with open(f"YOLO Model\\Model\\{model}\\{yolo_model}.yaml", 'w') as file:
-    yaml.dump(config, file, default_flow_style=False, sort_keys=False, allow_unicode=True)
+    yaml.safe_dump(config, file)
